4871f0063bc8c26279d80e301cd9eac4f40112a3
shimat <schimatk@gmail.com>
2023-06-06 08:48:29 +0900

    1st commit

---
+++ CommunityToolkitSamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Running;
+using CommunityToolkitSamples;
+
+/*var f = new MedianFilter();
+f.IterationSetup();
+f.Memory2d();
+f.ImageSharp();*/
+
+BenchmarkRunner.Run<MedianFilter>();
+
+
+/*
+
+var m = new Memory2D<byte>();
+using var h = m.Pin();
+
+
+var array = new float[10000];
+ParallelHelper.ForEach<float, ByTwoMultiplier>(array);
+array.ToString();
+
+public readonly struct ByTwoMultiplier : IRefAction<float>
+{
+    public void Invoke(ref float x) => x *= 2;
+}
+*/
